9acaf8084a2b1085a32def280ef0e89796a60ebd
Rafat Abdullah Pantho <rafatpantho@gmail.com>
2025-08-23 12:02:27 +0600

    Built the project with Bellsoft Liberica-21.0.8

---
+++ src/main/java/com/example/secondserve/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.secondserve;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
